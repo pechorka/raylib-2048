493604d55ccfc8618b8af9dd30cf67b4fbe541bb
pechorka <pechor44@gmail.com>
2024-03-10 15:59:33 +0500

    render on screen controls

--- game.c
+++ game.c
@@ -6,14 +6,75 @@
     #include <emscripten/emscripten.h>
 #endif
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+
+#define BOARD_DIMENTION 4
+#define CELL_SIZE 60
+#define CELL_TEXT_SIZE 20
+
+#define BOARD_X (SCREEN_WIDTH - BOARD_DIMENTION * CELL_SIZE) / 2
+#define BOARD_Y (SCREEN_HEIGHT - BOARD_DIMENTION * CELL_SIZE) / 2
+#define BOARD_WIDTH (BOARD_DIMENTION * CELL_SIZE)
+#define BOARD_HEIGHT BOARD_WIDTH
+
+Rectangle boardRect = {
+    .x = BOARD_X,
+    .y = BOARD_Y,
+    .width = BOARD_WIDTH,
+    .height = BOARD_HEIGHT
+};
+
+#define CONTROL_SIZE 40
+#define CONTROL_TEXT_SIZE 30
+#define CONTROL_PADDING 10
+#define CONTROL_SIZE_WITH_PADDING (CONTROL_SIZE + CONTROL_PADDING*2)
+
+#define CONTROL_PANEL_X (SCREEN_WIDTH - CONTROL_PANEL_WIDTH) / 2
+#define CONTROL_PANEL_Y (BOARD_Y + BOARD_HEIGHT + 20)
+#define CONTROL_PANEL_WIDTH (CONTROL_SIZE_WITH_PADDING * 3)
+#define CONTROL_PANEL_HEIGHT (CONTROL_SIZE_WITH_PADDING * 2)
+
+Rectangle controlRect = {
+    .x = CONTROL_PANEL_X,
+    .y = CONTROL_PANEL_Y,
+    .width = CONTROL_PANEL_WIDTH,
+    .height = CONTROL_PANEL_HEIGHT
+};
+
+Rectangle upControlRect = {
+    .x = (CONTROL_PANEL_WIDTH - CONTROL_SIZE) / 2 + CONTROL_PANEL_X,
+    .y = CONTROL_PANEL_Y + CONTROL_PADDING,
+    .width = CONTROL_SIZE,
+    .height = CONTROL_SIZE
+};
+
+Rectangle downControlRect = {
+    .x = (CONTROL_PANEL_WIDTH - CONTROL_SIZE) / 2 + CONTROL_PANEL_X,
+    .y = CONTROL_PANEL_Y + CONTROL_PANEL_HEIGHT - CONTROL_SIZE - CONTROL_PADDING,
+    .width = CONTROL_SIZE,
+    .height = CONTROL_SIZE
+};
+
+Rectangle leftControlRect = {
+    .x = CONTROL_PANEL_X + CONTROL_PADDING,
+    .y = (CONTROL_PANEL_HEIGHT - CONTROL_SIZE) / 2 + CONTROL_PANEL_Y,
+    .width = CONTROL_SIZE,
+    .height = CONTROL_SIZE
+};
+
+Rectangle rightControlRect = {
+    .x = CONTROL_PANEL_X + CONTROL_PANEL_WIDTH - CONTROL_SIZE - CONTROL_PADDING,
+    .y = (CONTROL_PANEL_HEIGHT - CONTROL_SIZE) / 2 + CONTROL_PANEL_Y,
+    .width = CONTROL_SIZE,
+    .height = CONTROL_SIZE
+};
+
 void UpdateDrawFrame(void);
 void reset_game(void);
 
 int main(void) {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-
-    InitWindow(screenWidth, screenHeight, "Hello, World!");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello, World!");
     reset_game();
 
     #if defined(PLATFORM_WEB)
@@ -34,12 +95,6 @@ int main(void) {
     return 0;
 }
 
-
-#define BOARD_DIMENTION 4
-#define CELL_SIZE 60
-#define CELL_TEXT_SIZE 20
-#define BOARD_SIZE (BOARD_DIMENTION * CELL_SIZE)
-
 int board[BOARD_DIMENTION][BOARD_DIMENTION];
 int score;
 
@@ -55,24 +110,21 @@ int random_2_power(int max) {
 
 int place_at_random_free_slot(int value) {
     if (freeSlotsCount == 0) {
-        printf("No free slots\n");
+        TraceLog(LOG_WARNING, "No free slots to place %d", value);
         return 0;
     }
 
     int nth_slot = rand() % freeSlotsCount+1;
-    printf("free_slots_count: %d\n", freeSlotsCount);
-    printf("nth_slot: %d\n", nth_slot);
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
+    for (int i = 0; i < BOARD_DIMENTION; i++) {
+        for (int j = 0; j < BOARD_DIMENTION; j++) {
             if (board[i][j] == 0 && --nth_slot == 0) {
                 board[i][j] = value;
                 freeSlotsCount--;
-                printf("Placed %d at %d, %d\n", value, i, j);
                 return 1;
             }
         }
     }
-    printf("Failed to place %d, %d\n", value, nth_slot);
+    TraceLog(LOG_WARNING, "Failed to place %d", value);
     return -1; // should never happen
 }
 
@@ -117,21 +169,21 @@ Color get_color(int value) {
     return (Color){r, g, b, 255};
 }
 
-void render_board(void) {
-    // render board border
-    Rectangle board_rect = {
-        .x = (GetScreenWidth() - BOARD_SIZE) / 2,
-        .y = (GetScreenHeight() - BOARD_SIZE) / 2,
-        .width = BOARD_SIZE,
-        .height = BOARD_SIZE
-    };
-    DrawRectangleLinesEx(board_rect, 2, BLACK);
+void render_text_at_center_of_rect(char* text, Rectangle rect, int fontSize, Color color) {
+    int textWidth = MeasureText(text, fontSize);
+    int textX = rect.x + rect.width / 2 - textWidth / 2;
+    int textY = rect.y + rect.height / 2 - fontSize / 2;
+    DrawText(text, textX, textY, fontSize, color);
+}
+
+void render_game(void) {
+    DrawRectangleLinesEx(boardRect, 2, BLACK);
     // render cells
     for (int i = 0; i < BOARD_DIMENTION; i++) {
         for (int j = 0; j < BOARD_DIMENTION; j++) {
             int value = board[i][j];
-            int x = board_rect.x + j * CELL_SIZE;
-            int y = board_rect.y + i * CELL_SIZE;
+            int x = boardRect.x + j * CELL_SIZE;
+            int y = boardRect.y + i * CELL_SIZE;
             Rectangle cell_rect = {
                 .x = x,
                 .y = y,
@@ -146,23 +198,33 @@ void render_board(void) {
             }
             char text[10];
             sprintf(text, "%d", value);
-            int textWidth = MeasureText(text, CELL_TEXT_SIZE);
-            // find center of the cell
-            int textX = x + CELL_SIZE / 2 - textWidth / 2;
-            int textY = y + CELL_SIZE / 2 - CELL_TEXT_SIZE / 2;
-            DrawText(text, textX, textY, CELL_TEXT_SIZE, BLACK);
+            render_text_at_center_of_rect(text, cell_rect, CELL_TEXT_SIZE, BLACK);
         }
     }
 
     // render score above the board
     char scoreText[50];
     sprintf(scoreText, "Score: %d", score);
-    DrawText(scoreText, board_rect.x, board_rect.y - 40, 30, BLACK);
+    DrawText(scoreText, boardRect.x, boardRect.y - 40, 30, BLACK);
     // draw best score above score
     char bestScoreText[50];
     sprintf(bestScoreText, "Best: %d", bestScore);
-    DrawText(bestScoreText, board_rect.x, board_rect.y - 80, 30, BLACK);
+    DrawText(bestScoreText, boardRect.x, boardRect.y - 80, 30, BLACK);
+
+    // render control panel
+    DrawRectangleLinesEx(controlRect, 2, BLACK);
+
+    DrawRectangleLinesEx(upControlRect, 2, BLACK);
+    render_text_at_center_of_rect("^", upControlRect, CONTROL_TEXT_SIZE, BLACK);
+
+    DrawRectangleLinesEx(downControlRect, 2, BLACK);
+    render_text_at_center_of_rect("v", downControlRect, CONTROL_TEXT_SIZE, BLACK);
     
+    DrawRectangleLinesEx(leftControlRect, 2, BLACK);
+    render_text_at_center_of_rect("<", leftControlRect, CONTROL_TEXT_SIZE, BLACK);
+
+    DrawRectangleLinesEx(rightControlRect, 2, BLACK);
+    render_text_at_center_of_rect(">", rightControlRect, CONTROL_TEXT_SIZE, BLACK);
 }
 
 void move_cells_left() {
@@ -253,40 +315,51 @@ void transpose_board() {
     }
 }
 
+int is_button_pressed(Rectangle rect) {
+    Vector2 mousePos = GetMousePosition();
+    return IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mousePos, rect);
+}
+
 int handle_input(void) {
     if (IsKeyPressed(KEY_R)) {
         reset_game();
         return 0;
     }
 
-    switch (GetKeyPressed())
+    if (IsKeyPressed(KEY_LEFT) || is_button_pressed(leftControlRect))
     {
-    case KEY_LEFT:
         move_cells_left();
         merge_cells_left();
         move_cells_left();
         return 1;
-    case KEY_RIGHT:
+    }
+
+    if (IsKeyPressed(KEY_RIGHT) || is_button_pressed(rightControlRect))
+    {
         move_cells_right();
         merge_cells_right();
         move_cells_right();
         return 1;
-    case KEY_UP:
+    }
+
+    if (IsKeyPressed(KEY_UP) || is_button_pressed(upControlRect))
+    {
         transpose_board();
         move_cells_left();
         merge_cells_left();
         move_cells_left();
         transpose_board();
         return 1;
-    case KEY_DOWN:
+    }
+
+    if (IsKeyPressed(KEY_DOWN) || is_button_pressed(downControlRect))
+    {
         transpose_board();
         move_cells_right();
         merge_cells_right();
         move_cells_right();
         transpose_board();
         return 1;
-    default:
-        break;
     }
 
     return 0;
@@ -304,7 +377,7 @@ void UpdateDrawFrame(void) {
 
     if (handle_input()) place_at_random_free_slot(random_2_power(maxCurrent2Power));
     
-    render_board();
+    render_game();
 
     EndDrawing();
 }
